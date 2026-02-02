12eaad80f85ee2f7995769ccf19f949e87ca85dc
Pika G <162190610+pikaachi@users.noreply.github.com>
2026-02-02 12:31:58 -0600

    update hero intro

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -13,9 +13,12 @@ const Hero = () => {
       <div className='hero-content'>
         <h1>hi,  <span className="pika-name">pika</span> here.</h1>
         <h2>I create stuff sometimes</h2>
-        <p>I'm a software engineer from Austin, TX. I'm facinated by Machine Learning (like all SE these days!) and 
-          I love to create interactive web applications. I'm currently looking for new opportunities.
-        </p>
+        <p>I'm a software engineer based in Austin, TX. 
+        I’m someone who’s endlessly curious about AI agents and 
+        how things can work just a little smarter. 
+        I also have a habit of optimizing everyday life—like 
+        subconsciously running a shortest-path algorithm (Dijkstra or A*) 
+        in my head while driving, factoring in distance, traffic lights, and traffic just for fun.</p>
         <a href="mailto:20dipikagiri21@gmail.com" className="hero-button">
           <FaEnvelope className="email-icon" />Say Hi
         </a>
